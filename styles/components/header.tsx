9fd4e1bbd84f14d4ed15aa184594d64a90c0c98d
Alan Alban <alan.alban10@gmail.com>
2022-06-12 16:41:22 -0400

    Added file structure and layout basics

---
+++ styles/components/header.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const NavBar = styled.nav`
+  width: 100%;
+  height: 100px;
+`;
+
+export const HeaderMenu = styled.ul`
+  color: orange;
+`;
+
+export const MenuItem = styled.li`
+  color: orange;
+`;
+
+export const Logo = styled.img`
+  color: orange;
+`;
