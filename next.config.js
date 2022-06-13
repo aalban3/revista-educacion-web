6e5fa9bdf298a5383bad53f1dff55b3b25f2456e
Alan Alban <alan.alban10@gmail.com>
2022-06-12 21:50:27 -0400

    Added babel config file to use styled components

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [process.env.NEXTJS_AWS_DOMAIN_1, process.env.NEXTJS_AWS_DOMAIN_2],
+  },
+  env: {
+    S3_BUCKET_URL: process.env.S3_BUCKET_URL,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
